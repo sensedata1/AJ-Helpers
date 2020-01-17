3b167ddd4a3f8be076b5483f401f38ba4f88bf1c
sensedata1 <lordben@gmail.com>
2020-01-17 23:35:39 +0000

    format code

--- wav2mp3dumper.sh
+++ wav2mp3dumper.sh
@@ -4,7 +4,7 @@
 
 # Redcursively convert all .wav files to .mp3, 
 # renaming to _.mp3 to avoid overwriting existing files 
-# then movees contents of $AJ_TEMP_DIR to $DUMP_DIR 
+# then move contents of $AJ_TEMP_DIR to $DUMP_DIR 
 # before deleting .wav files 
 
 
@@ -13,8 +13,11 @@ DUMP_DIR='/Users/blord/Dropbox/DUMPER'
 
 cd "$AJ_TEMP_DIR"
 
-find . -type f -name *.wav -exec bash -c ' ffmpeg \
-  -i "$0" -c:a libmp3lame \
+find . \
+  -type f \
+  -name *.wav \
+  -exec bash -c ' \
+  ffmpeg -i "$0" -c:a libmp3lame \
   -q:a 1 "${0/%.wav/_.mp3}" ' {} \;
 
 mv -f  "$AJ_TEMP_DIR"/* "$DUMP_DIR"
